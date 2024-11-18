7f4161c64e4e0f91058c302f41d5c99b3933bb72
Niall Oswald <niall.oswald@hotmail.co.uk>
2024-11-18 16:00:47 +0000

    Feat: Implement exercise_3_2.py

---
+++ exercises/exercise_3_2.py
@@ -0,0 +1,57 @@
+"""Exercise 3.2."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def poisson_1d(S, alpha, beta, mesh, node_map):
+    # Compute the local stiffness matrix
+    K_loc = lambda a, b: np.array([[1, -1], [-1, 1]]) / (b - a)
+    f_loc = lambda a, b: (b - a) / 6 * np.array([2 * S(a) + S(b), S(a) + 2 * S(b)])
+
+    # Compute the global stiffness matrix
+    K = np.zeros((len(mesh), len(mesh)))
+    f = np.zeros_like(mesh)
+    for e in node_map:
+        K[np.ix_(e, e)] += K_loc(*mesh[e])
+        f[e] += f_loc(*mesh[e])
+
+    # Set the dirichlet boundary conditions
+    K[0] = 0
+    K[0, 0] = 1
+    f[0] = alpha
+
+    # Set the neumann boundary conditions
+    f[-1] += beta
+
+    # Solve the system
+    c = np.linalg.solve(K, f)
+
+    return c
+
+
+def __main__():
+    # Parameters
+    N_ELEMS = 100
+    S = lambda x: (1 - x) ** 2  # noqa: E731
+
+    # Generate a uniform mesh
+    mesh = np.linspace(0, 1, N_ELEMS + 1)
+
+    # Setup the node map
+    node_order = np.arange(N_ELEMS, dtype=np.int64)[:, np.newaxis]
+    node_map = np.hstack([node_order, node_order + 1])
+
+    # Solve the system
+    psi = poisson_1d(S, 0, 0, mesh, node_map)
+
+    # Plot the solution
+    plt.plot(mesh, psi, "k-")
+    plt.xlabel(r"$x$")
+    plt.ylabel(r"$\psi(x)$")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    __main__()
