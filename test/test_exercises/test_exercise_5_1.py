bd62fd2748cdb2a113ecad386f31ec20190b49b3
Niall Oswald <niall.oswald@hotmail.co.uk>
2024-11-27 11:52:12 +0000

    Test: Update tests to use optimized function

--- test/test_exercises/test_exercise_5_1.py
+++ test/test_exercises/test_exercise_5_1.py
@@ -3,11 +3,10 @@
 from exercises.exercise_3_2 import poisson_1d
 from exercises.exercise_5_1 import dt_advection_diffusion_1d
 import numpy as np
-import matplotlib.pyplot as plt
 import pytest
 
 
-@pytest.mark.parametrize("t_final, tol", zip([0.5, 1.0, 2.0], [1e-2, 1e-2, 1e-2]))
+@pytest.mark.parametrize("t_final, tol", zip([2.5, 5.0], [1e-3, 1e-4]))
 def test_steady_state(t_final, tol, N_ELEMS=20, dt=1e-4):
     # Parameters
     S = lambda x: 1.0 - x
@@ -25,21 +24,15 @@ def test_steady_state(t_final, tol, N_ELEMS=20, dt=1e-4):
     node_order = np.arange(N_ELEMS, dtype=np.int64)[:, np.newaxis]
     node_map = np.hstack([node_order, node_order + 1])
 
+    # Cache the time derivative
+    args = (S, u, mu, alpha, beta, mesh, node_map)
+    psi_dt = dt_advection_diffusion_1d("optimize", *args)
+
     # Solve the system using the method of lines
     psi = np.zeros_like(mesh)
-    args = (S, u, mu, alpha, beta, mesh, node_map)
     for i in range(int(t_final / dt)):
-        psi_step = psi + dt * dt_advection_diffusion_1d(psi, *args)
-        psi = (psi + psi_step + dt * dt_advection_diffusion_1d(psi_step, *args)) / 2
-
-    plt.plot(mesh, psi, "k-", label=r"$\psi(x, t)$")
-    plt.plot(
-        mesh, poisson_1d(S, alpha, beta, mesh, node_map), "k--", label=r"$\psi(x)$"
-    )
-    plt.xlabel(r"$x$")
-    plt.ylabel(r"$\psi(x, t)$")
-    plt.legend()
-    plt.show()
+        psi_step = psi + dt * psi_dt(psi)
+        psi = (psi + psi_step + dt * psi_dt(psi_step)) / 2
 
     # Check the solution
     assert np.allclose(psi, poisson_1d(S, alpha, beta, mesh, node_map), atol=tol)
