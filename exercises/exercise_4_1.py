Fix: Add missing absolute value to Jacobian determinant

exercises/exercise_4_1.py
@@ -83,7 +83,7 @@ def K_loc(*e):
             grad_phi(),
             J_inv(*e),
         )
-        * np.linalg.det(J(*e))
+        * abs(np.linalg.det(J(*e)))
         / 2
     )
 
@@ -95,7 +95,7 @@ def f_loc(S, *e):
             [phi(xi) * S(global_map(xi, *e)) for xi in gauss_points],
             axis=0,
         )
-        * np.linalg.det(J(*e))
+        * abs(np.linalg.det(J(*e)))
         / 6
     )
 