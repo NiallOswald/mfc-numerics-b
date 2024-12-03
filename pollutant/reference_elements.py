ef067d0a2e01cfa919d3f4600b6ffd8e7bfc93c5
Niall Oswald <niall.oswald@hotmail.co.uk>
2024-12-03 22:18:27 +0000

    Fix: Correct normal vector computation

--- pollutant/reference_elements.py
+++ pollutant/reference_elements.py
@@ -2,10 +2,14 @@ import numpy as np
 
 
 class ReferenceCell:
-    def __init__(self, vertices):
+    def __init__(self, vertices, cell_normals):
         self.vertices = vertices
+        self.cell_normals = cell_normals
         self.dim = self.vertices.shape[1]
 
 
-ReferenceInterval = ReferenceCell(np.array([[0.0], [1.0]]))
-ReferenceTriangle = ReferenceCell(np.array([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0]]))
+ReferenceInterval = ReferenceCell(np.array([[0.0], [1.0]]), None)
+ReferenceTriangle = ReferenceCell(
+    np.array([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0]]),
+    np.array([[0.0, -1.0], [1.0 / np.sqrt(2), 1.0 / np.sqrt(2)], [-1.0, 0.0]]),
+)
