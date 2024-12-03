8708b5ae9bed26bba3d28e9718a996aa21b3ab46
Niall Oswald <niall.oswald@hotmail.co.uk>
2024-12-03 22:46:04 +0000

    Feat: Add `utils.find_element`

--- pollutant/utils.py
+++ pollutant/utils.py
@@ -1,6 +1,7 @@
 """Utilities for the finite element method."""
 
 import numpy as np
+from scipy.optimize import linprog
 
 
 def load_mesh(name: str, scale: str):
@@ -21,3 +22,20 @@ def gaussian_source(x, x0, amplitude=1.0, radius=1.0, order=2.0):
     )
     val[np.linalg.norm(x - x0, axis=1) > radius] = 0.0
     return np.nan_to_num(val, nan=0.0)
+
+
+def find_element(x, nodes, node_map):
+    """Find the element containing the point x."""
+    for i, element in enumerate(node_map):
+        if is_inside(x, nodes[element]):
+            return i
+    else:
+        raise ValueError("Point not found in any element.")
+
+
+def is_inside(x, points):
+    """Check if the point x is inside the convex hull of the points."""
+    A_eq = np.vstack([points.T, np.ones(len(points))])
+    b_eq = np.array([*x, 1])
+    cost = np.zeros(len(points))
+    return linprog(cost, A_eq=A_eq, b_eq=b_eq).success
