Fix: Remove absolute values for Jacobian determinant

exercises/exercise_4_1.py
@@ -46,7 +46,7 @@ def K_loc(*e):
             grad_phi(),
             J_inv(*e),
         )
-        * abs(np.linalg.det(J(*e)))
+        * np.linalg.det(J(*e))
         / 2
     )
 
@@ -58,7 +58,7 @@ def f_loc(S, *e):
             [phi(xi) * S(global_map(xi, *e)) for xi in gauss_points],
             axis=0,
         )
-        * abs(np.linalg.det(J(*e)))
+        * np.linalg.det(J(*e))
         / 6
     )
 

pollutant/solvers/advection_diffusion.py
@@ -36,7 +36,7 @@ def dt_advection_diffusion(c, S, u, kappa, mesh, node_map, boundaries):
     for nodes in alive_it(node_map, title="Construcing stiffness matrix..."):
         J = np.einsum("ja,jb", mesh[nodes], fe.cell_jacobian, optimize=True)
         inv_J = np.linalg.inv(J)
-        det_J = abs(np.linalg.det(J))
+        det_J = np.linalg.det(J)
 
         M[np.ix_(nodes, nodes)] += (
             np.einsum(