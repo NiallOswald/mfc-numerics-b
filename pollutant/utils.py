Fix: Reduce gaussian gradients for larger radii

pollutant/utils.py
@@ -17,8 +17,10 @@ def load_mesh(name: str, scale: str):
 
 def gaussian_source(x, x0, amplitude=1.0, radius=1.0, order=2.0):
     """A gaussian source term."""
-    val = amplitude * np.exp(
-        -1 / (radius**order - np.linalg.norm(x - x0, axis=1) ** order)
+    val = (
+        amplitude
+        * np.e
+        * np.exp(-1 / (1 - np.linalg.norm((x - x0) / radius, axis=1) ** order))
     )
     val[np.linalg.norm(x - x0, axis=1) > radius] = 0.0
     return np.nan_to_num(val, nan=0.0)