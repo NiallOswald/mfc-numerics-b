23e85a842dd91ce185ac8f7dc981fe500dee4ca2
Niall Oswald <niall.oswald@hotmail.co.uk>
2024-12-09 21:41:35 +0000

    Feat: Add scripts to download and process historical weather data

---
+++ pollutant/scripts/process_weather_data.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+from alive_progress import alive_it
+from argparse import ArgumentParser
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+YEAR = "2005"
+PATTERN = f"**/qc-version-1/*{YEAR}.csv"
+HEADER = 80  # Metadata rows to skip
+
+
+def knots_to_mps(knots):
+    """Convert knots to meters per second."""
+    return knots * 0.514444
+
+
+knots_to_mps = np.vectorize(knots_to_mps)
+
+
+def post_process(df):
+    """Convert wind speed and direction to horizontal and vertical components."""
+    df["mean_wind_speed"] = knots_to_mps(df["mean_wind_speed"])
+    df["mean_wind_dir"] = np.radians(df["mean_wind_dir"])
+    df["horizontal_wind_speed"] = df["mean_wind_speed"] * np.sin(df["mean_wind_dir"])
+    df["vertical_wind_speed"] = df["mean_wind_speed"] * np.cos(df["mean_wind_dir"])
+
+    df.drop(columns=["mean_wind_dir", "mean_wind_speed"], inplace=True)
+
+
+def process_weather_data():
+    parser = ArgumentParser()
+    parser.add_help = """Process MetOffice MIDAS Open: UK mean wind data.
+
+    Data is available at: https://dx.doi.org/10.5285/91cb9985a6c2453d99084bde4ff5f314.
+    """
+    parser.add_argument("path", type=str, help="Path to the data.")
+    parser.add_argument(
+        "--start",
+        type=str,
+        default="2005-10-30 06:00:00",
+        help="Start of datetime range to include.",
+    )
+    parser.add_argument(
+        "--end", type=str, default="2005-10-31 06:00:00", help="End of datetime range."
+    )
+    parser.add_argument(
+        "--output", type=str, default="wind_data.csv", help="Output path for the CSV."
+    )
+    args = parser.parse_args()
+
+    path = Path(args.path)
+    start = args.start
+    end = args.end
+    output = args.output
+
+    pathlist = path.glob(PATTERN)
+
+    dataframes = []
+    for path in alive_it(pathlist, title="Searching for files..."):
+        with open(path, "r") as csvfile:
+            df = pd.read_csv(csvfile, header=HEADER, index_col=0)
+            df = df[start:end]
+
+            dataframes.append(df)
+
+    df = pd.concat(dataframes)
+    df.reset_index(inplace=True)
+    df = df[
+        ["ob_end_time", "src_id", "mean_wind_dir", "mean_wind_speed"]
+    ]  # Drop unnecessary columns
+
+    post_process(df)
+
+    df.to_csv(output)
