4196658866b6ac0565ce2ea48fcc1b2c26b4ad28
Niall Oswald <niall.oswald@hotmail.co.uk>
2024-12-18 16:24:19 +0000

    Chore: Update parameters

--- pollutant/constants.py
+++ pollutant/constants.py
@@ -6,7 +6,7 @@ import numpy as np
 SOUTHAMPTON = np.array([442365.0, 115483.0])
 READING = np.array([473993.0, 171625.0])
 
-BURN_TIME = 8.0 * 60.0**2.0
+BURN_TIME = 10.0 * 60.0**2.0
 FIRE_START = datetime(2005, 10, 30, 6, 0, 0)
 
 DEFAULT_WIND_SPEED = [0.0, 10.0]
